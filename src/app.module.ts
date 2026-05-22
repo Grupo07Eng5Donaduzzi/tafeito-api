8af06513a70db296384d3d85fa6d0238d5f02c57
Gabryel <gabryelpaiva123@gmail.com>
2026-05-21 22:48:06 -0300

    feat(security): add global ThrottlerGuard (100 req/60s)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { SharedModule } from '@shared/shared.module';
 import { UsersModule } from '@users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ServicesModule } from './modules/services/services.module';
 
 @Module({
-  imports: [SharedModule, UsersModule, AuthModule, ServicesModule],
+  imports: [
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    SharedModule,
+    UsersModule,
+    AuthModule,
+    ServicesModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
